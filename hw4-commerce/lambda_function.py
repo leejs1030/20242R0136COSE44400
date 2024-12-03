fe0464d1e1182d432eecef08aff493f7139db343
leejs1030 <leejs1030@korea.ac.kr>
2024-12-03 23:48:20 +0900

    lambda_function 완성

--- hw4-commerce/lambda_function.py
+++ hw4-commerce/lambda_function.py
@@ -2,42 +2,55 @@ import boto3
 from textblob import TextBlob
 import datetime
 
-dynamodb = boto3.resource('dynamodb')
-table = dynamodb.Table('reviews')
-ses = boto3.client('ses')
+student_number = "2020320125"
+
+dynamodb = boto3.resource("dynamodb")
+table = dynamodb.Table(f"reviews-{student_number}")
+ses = boto3.client("ses")
+
+
+def get_sentiment(polarity):
+    if polarity > 0:
+        return "Positive"
+    elif polarity < 0:
+        return "Negative"
+    else:
+        return "Neutral"
+
 
 def lambda_handler(event, context):
-    user_name = event['user_name']
-    review_text = event['review']
+    user_name = event["user_name"]
+    review_text = event["review"]
     timestamp = datetime.datetime.now().isoformat()
 
     # Sentiment Analysis
     polarity = TextBlob(review_text).sentiment.polarity
-    '''
+    """
     Todo1: Use the Polarity value to determine the sentiment of the review, 
     with the standard set to 0.
-    '''
-
+    """
+    sentiment = get_sentiment(polarity)
 
     # Save to DynamoDB
     table.put_item(
         Item={
-            '''
-            Todo2: Fill in the Item dictionary with the keys and values
-                    required to store the review in the DynamoDB table.
-            '''
+            "user_name": user_name,
+            "review": review_text,
+            "timestamp": timestamp,
+            "sentiment": sentiment,
         }
     )
 
     # Send Email for Positive Reviews
+    my_email = "leejs1030@korea.ac.kr"
     if sentiment == "Positive":
         ses.send_email(
-            Source="verified email address1",
-            Destination={"ToAddresses": ["verified email address2"]},
+            Source=my_email,
+            Destination={"ToAddresses": [my_email]},
             Message={
                 "Subject": {"Data": f"Positive Review from {user_name}"},
-                "Body": {"Text": {"Data": f"{user_name} wrote: {review_text}"}}
-            }
+                "Body": {"Text": {"Data": f"{user_name} wrote: {review_text}"}},
+            },
         )
 
     return {"statusCode": 200, "body": f"Review processed for {user_name}"}
